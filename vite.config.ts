e0b8edadbcd5005000a904088a9c2f2bba3642ce
ndntan911 <ndntan911@gmail.com>
2026-02-02 21:23:59 +0700

    update page

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: process.env.NODE_ENV === 'production' ? '/personal-portfolio/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/portfolio/' : '/',
   build: {
     outDir: 'dist',
     sourcemap: true,
